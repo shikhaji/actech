57811c9a984d75f0b56ac715e2a1fac7d5eeb382
patelnandan <nandanttt@gmail.com>
2023-02-11 23:54:26 +0530

    Updated[11-2-23]

--- lib/routes/arguments.dart
+++ lib/routes/arguments.dart
@@ -2,6 +2,9 @@ class OtpArguments {
   String? phoneNumber;
   int? otpStatus;
   String? userId;
+  String? ccId;
+  String? ccImg;
+  String? ccName;
 
-  OtpArguments({this.phoneNumber, this.otpStatus, this.userId});
+  OtpArguments({this.phoneNumber, this.otpStatus, this.userId, this.ccId, this.ccImg, this.ccName});
 }
